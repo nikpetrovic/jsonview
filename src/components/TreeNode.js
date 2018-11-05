b3b5968782283bbe622d5c59184ca6456dae28d4
Nikola Petrovic <nikolapetrovic@MacBook-Pro.local>
2018-11-05 20:37:03 +0100

    formatting values

--- src/components/TreeNode.js
+++ src/components/TreeNode.js
@@ -1,12 +1,53 @@
 import React from 'react'
+import map from 'lodash/map'
+import './TreeNode.scss'
+
+const SIMPLE = 'simple'
+const COMPLEX = 'complex'
 
 export default class TreeNode extends React.Component {
 	constructor(props) {
 		super(props)
 	}
 
+	getValueType = value => {
+		if (typeof value === 'string' || typeof value === 'number') {
+			return SIMPLE
+		} else if (typeof value === 'object') {
+			return COMPLEX
+		}
+
+		return undefined
+	}
+
+	renderValue = (value, valueType) => {
+		if (valueType === SIMPLE) {
+			return <div className="value">{value}</div>
+		} else if (valueType === COMPLEX) {
+			return map(value, (v, k) => <TreeNode key={k} name={k} value={v} />)
+		}
+
+		return null
+	}
+
 	render() {
 		const { name, value } = this.props
-		return <div>{name}</div>
+		const valueType = this.getValueType(value)
+		return (
+			<div
+				className={`tree-node${
+					valueType === COMPLEX ? ' complex' : ''
+				}`}
+			>
+				<div className="name">{name}:</div>
+				<div
+					className={`value${
+						valueType === COMPLEX ? ' complex' : ''
+					}`}
+				>
+					{this.renderValue(value, valueType)}
+				</div>
+			</div>
+		)
 	}
 }
